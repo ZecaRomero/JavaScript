6c97f956df3b11be86ec829932646e3bbb6a9d65
Zeca Romero <zeca.85@gmail.com>
2024-04-10 09:00:57 -0300

    1

---
+++ teste.js
@@ -0,0 +1,9 @@
+alert('Ola Seja bem vindo ao curso de JS !')
+document.getElementById('nome').value = 'oi'
+
+
+//comentario de uma linha
+
+/* Cmentário de multiplas linhas
+esse tipo de comentario permite que varias linhas sejam encapsuladas
+em um mesmo comentario*/
